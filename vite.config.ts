a1a216bff7916809b3f2f74e64e3be55979d6b8b
Josip Benko-Đaković <josip.benkodakovic@barrage.net>
2023-06-19 14:24:14 +0200

    add prettier, add base dir to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
+  base: './',
   plugins: [svelte()],
-})
+});
