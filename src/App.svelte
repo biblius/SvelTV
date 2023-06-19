309161088d596f1cbd210a51398835a6a0e2e32a
Josip Benko-Đaković <josip.benkodakovic@barrage.net>
2023-06-19 15:47:40 +0200

    lint

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,10 @@
 <main>
 	<div>
 		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src={viteLogo} class="logo" alt="Vite Logo" />
+			<img src="{viteLogo}" class="logo" alt="Vite Logo" />
 		</a>
 		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+			<img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
 		</a>
 	</div>
 	<h1>Vite + Svelte</h1>
