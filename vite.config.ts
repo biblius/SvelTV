2f9094e8f11fd7076d389332a66f56890f7e0b29
Matej Kolarević <mkolarevic@protonmail.com>
2023-06-29 09:50:16 +0200

    Add vite-legacy to polyfill unsupported javascript on older platforms

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-		sourcemap: true
+		sourcemap: true,
+		cssTarget: ['chrome38']
 	},
 	base: './',
-	plugins: [svelte()]
+	plugins: [
+		svelte(),
+		legacy({
+			//targets: ['defaults', 'not IE 11']
+			targets: ['chrome 38']
+		})
+	]
 });
